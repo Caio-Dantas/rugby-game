db77dbe1f8c93eee968a480c72f3c68dc96b8f77
Caio Dantas <caiodantas13@gmail.com>
2022-03-24 20:57:02 -0300

    Implementa attacker e defender

--- src/attacker.c
+++ src/attacker.c
@@ -9,8 +9,30 @@
 // Main header
 #include "attacker.h"
 
-// Macros
-#define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+// CONSTS
+int ATTACKER_USE_SPY_MOMENT = 2;
+
+// State
+static position_t previous_attacker_position;
+static int turns = 0;
+static direction_t current_direction;
+
+bool is_position_equals(position_t position_1, position_t position_2){
+  return (position_1.i == position_2.i && position_1.j == position_2.j);
+}
+
+bool is_direction_equals_attacker(direction_t direction_1, direction_t direction_2){
+  return (direction_1.i == direction_2.i && direction_1.j == direction_2.j);
+}
+
+/**
+ * Choose the best attacker's direction to avoid defender
+ *  based on both positions (attacker and defender)
+ */
+direction_t get_best_attacker_direction_by_positions(position_t attacker_position, position_t defender_position){
+  if (attacker_position.j < defender_position.j) return (direction_t) DIR_DOWN_RIGHT;
+  return (direction_t) DIR_UP_RIGHT;
+}
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -18,12 +40,37 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  direction_t chosen_direction;
+
+  // Normal cases
+  if (turns == 0){ // Go right on first turn
+    chosen_direction = (direction_t) DIR_RIGHT;
+  } else { // Always keep up and right
+    chosen_direction = (direction_t) DIR_UP_RIGHT;
+  }
+
+  // Corner cases --> Cases when we found a corner
+  if(is_position_equals(previous_attacker_position, attacker_position)){
+    if(is_direction_equals_attacker(current_direction, (direction_t) DIR_UP_RIGHT)){ // We can't go up and right
+      chosen_direction = (direction_t) DIR_DOWN_RIGHT;
+    } else { // Always up and right
+      chosen_direction = (direction_t) DIR_UP_RIGHT;
+    }
+  }
+
+  // Spy cases
+  if(turns == ATTACKER_USE_SPY_MOMENT){ // Use spy based on const
+    position_t defender_position = get_spy_position(defender_spy);
+    chosen_direction = get_best_attacker_direction_by_positions(attacker_position, defender_position);
+  }
+
+  // Update state
+  previous_attacker_position = attacker_position;
+  turns += 1;
+  current_direction = chosen_direction;
+
+  return chosen_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -12,18 +12,64 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+// CONSTS
+int DEFENDER_USE_SPY_MOMENT = 2;
+int MAX_VERTICAL_CRAB_STEPS = 2;
+
+// State
+static int turns = 0;
+static direction_t current_direction;
+static bool vertical_crab = true;
+static int current_vertical_crab_steps = 0;
+
+bool is_direction_equals_defender(direction_t direction_1, direction_t direction_2){
+  return (direction_1.i == direction_2.i && direction_1.j == direction_2.j);
+}
+
+direction_t inverse_direction_vertical(direction_t direction){
+  if(is_direction_equals_defender(direction, (direction_t) DIR_UP)) return (direction_t) DIR_DOWN;
+  return (direction_t) DIR_UP;
+}
+
+/**
+ * Choose the best defender's direction to reach attacker
+ *  based on both positions (attacker and defender)
+ */
+direction_t get_best_defender_direction_by_positions(position_t defender_position, position_t attacker_position){
+  if (attacker_position.j < defender_position.j) return (direction_t) DIR_DOWN;
+  if (attacker_position.j > defender_position.j) return (direction_t) DIR_UP;
+  return (direction_t) DIR_LEFT;
+}
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  direction_t chosen_direction;
+
+  // Normal cases
+  if(vertical_crab && current_vertical_crab_steps == MAX_VERTICAL_CRAB_STEPS) { // Time to change directions
+    current_vertical_crab_steps = 0;
+    chosen_direction = inverse_direction_vertical(current_direction);
+  } else { // Keep crabing
+    current_vertical_crab_steps += 1;
+    chosen_direction = current_direction;
+  }
+
+  // Spy cases
+  if(turns == DEFENDER_USE_SPY_MOMENT){ // Use spy based on const
+    vertical_crab = false; // Get "smart" and disable crab strategy
+    position_t attacker_position = get_spy_position(attacker_spy);
+    chosen_direction = get_best_defender_direction_by_positions(defender_position, attacker_position);
+  }
+
+  // Update state
+  turns += 1;
+
+  return chosen_direction;
 }
 
 /*----------------------------------------------------------------------------*/
